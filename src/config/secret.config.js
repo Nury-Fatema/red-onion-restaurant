1ee837e29da27e0c6483f45481630578946a01c1
Nury-Fatema <nury.fatema082@gmail.com>
2020-03-23 06:40:40 +0600

    first commit

---
+++ src/config/secret.config.js
@@ -0,0 +1,10 @@
+//Firebase config DB
+export const firebaseConfig = {
+    apiKey: "AIzaSyD4kLJKFrqUHHT_pKMjbnaIeAOxHwn5OMw",
+    authDomain: "red-onion-restaurant-6fd0d.firebaseapp.com",
+    databaseURL: "https://red-onion-restaurant-6fd0d.firebaseio.com",
+    projectId: "red-onion-restaurant-6fd0d",
+    storageBucket: "red-onion-restaurant-6fd0d.appspot.com",
+    messagingSenderId: "732231631409",
+    appId: "1:732231631409:web:2c135237392bc3c5a58b10"
+  };
